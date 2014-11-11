ce21630428ab6396afce9dfb52621fd2005633f0
Andrew Hecht <andrewh@lightspeedsystems.com>
2014-11-11 15:41:22 -0800

    User create flow

--- web_service/models/user.rb
+++ web_service/models/user.rb
@@ -4,7 +4,24 @@ class User < OpenStruct
   dynamo_attribute :lat, :n
   dynamo_attribute :long, :n
 
-  def self.test(msg)
-    puts msg
+  def self.create(user_name, lat = nil, long = nil)
+    user = User.find(user_name) rescue nil
+    if user
+      user.lat = lat
+      user.long = long
+      user.save
+    else
+      user = User.new(user_name: user_name, lat: lat, long: long)
+      user.save
+    end
+    user.send_welcome
+  end
+
+  def send_welcome
+    if lat.present? && long.present?
+      Message.delay.send(self.user_name, "Thank you for subscribing to Atrocity Watch.")
+    else
+      Message.delay.send(self.user_name, "Please let us know where you are currently located.")
+    end
   end
 end
