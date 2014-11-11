f1872a2d4019752db9dd80c560e42668828ea641
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2014-11-11 10:48:38 -0800

    added identifiers for project

--- web_service/config/initializers/aws.rb
+++ web_service/config/initializers/aws.rb
@@ -1,9 +1,9 @@
 require 'aws-sdk'
 
 Aws.config[:s3] = {
-  :region => 'us-west-1',
-  :access_key_id =>  ENV['AWS_KEY'],
-  :secret_access_key =>  ENV['AWS_SECRET']
+  :region => ENV['AWW_REGION'],
+  :access_key_id =>  ENV['AWW_AWS_KEY'],
+  :secret_access_key =>  ENV['AWW_AWS_SECRET']
 }
 $s3 = Aws::S3.new
 $sns = Aws::SNS::Client.new
