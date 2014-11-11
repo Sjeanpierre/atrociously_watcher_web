26946d434ae76a69d03baa1e8606ff2e056b50c2
Andrew Hecht <andrewh@lightspeedsystems.com>
2014-11-11 12:36:33 -0800

    dynamo model.

--- web_service/config/initializers/dynamo_model.rb
+++ web_service/config/initializers/dynamo_model.rb
@@ -4,24 +4,6 @@ module ODynamoDb
     base.extend(ClassMethods)
   end
 
-  module ClassMethods
-    def dynamo_attribute(name, type)
-      dynamo_db_fields = self.class_variable_get(:@@dynamo_db_fields)
-      dynamo_db_fields[name] = type
-    end
-  end
-
-  def dynamo_attributes
-    attrs = Hash.new
-    dynamo_db_fields = self.class.class_variable_get(:@@dynamo_db_fields)
-    dynamo_db_fields.each do |name, type|
-      val = Hash.new
-      val[type] = self.send(name)
-      attrs[name] = val
-    end
-    return attrs
-  end
-
   def self.configure_options(klass, options = {})
     [:dynamo_table_name, :dynamo_hash_key].each do |name|
       klass.class_eval %Q{
@@ -37,6 +19,49 @@ module ODynamoDb
         klass.send("#{name}=", value)
       end
     end
+  end
+
+  module ClassMethods
+    def dynamo_attribute(name, type)
+      dynamo_db_fields = self.class_variable_get(:@@dynamo_db_fields)
+      dynamo_db_fields[name] = type
+    end
+
+    def find(id)
+      res = $dynamo_db.get_item(
+        table_name: self.dynamo_table_name, 
+        key: {self.dynamo_hash_key => id}
+      )
+      if res.item
+        self.new(res.item)
+      else
+        raise 'Record not found'
+      end
+    end
+  end
+
+  def dynamo_attributes
+    attrs = Hash.new
+    dynamo_db_fields = self.class.class_variable_get(:@@dynamo_db_fields)
+    dynamo_db_fields.each do |name, type|
+      val = self.send(name)
+      if type == :s
+        val = val.to_s
+      end
+      attrs[name.to_s] = val
+    end
+    return attrs
+  end
+
+  def save
+    attrs = dynamo_attributes
+    unless hash_key = attrs[self.class.dynamo_hash_key.to_s].present?
+      raise 'No Key value to save with.'
+    end
+    $dynamo_db.put_item(
+      table_name: self.class.dynamo_table_name,
+      item: attrs
+    )
   end
 end
 
