70daaf90e50934ad8258b65e9943c14f8ef7de35
Andrew Hecht <andrewh@lightspeedsystems.com>
2014-11-11 12:04:47 -0800

    turn on dynamo

--- web_service/config/initializers/aws.rb
+++ web_service/config/initializers/aws.rb
@@ -1,10 +1,11 @@
 require 'aws-sdk'
 
-Aws.config[:s3] = {
+opts = {
   :region => ENV['AWW_REGION'],
   :access_key_id =>  ENV['AWW_AWS_KEY'],
   :secret_access_key =>  ENV['AWW_AWS_SECRET']
 }
+
 # $s3 = Aws::S3::Client.new
 # $sns = Aws::SNS::Client.new
-# $dynamo_db = Aws::DynamoDB::Client.new
+$dynamo_db = Aws::DynamoDB::Client.new(opts)
