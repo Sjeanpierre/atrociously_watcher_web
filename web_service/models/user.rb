45dd5e7ee15bd8b90022cc7413d7a9462d88f808
Andrew Hecht <andrewh@lightspeedsystems.com>
2014-11-11 12:02:02 -0800

    add lat and long

--- web_service/models/user.rb
+++ web_service/models/user.rb
@@ -1,4 +1,6 @@
 class User < OpenStruct
-  acts_as_dynamo_db(dynamo_hash_key: :user_name)
-  dynamo_attribute(:user_name, :s)
+  acts_as_dynamo_db dynamo_hash_key: :user_name 
+  dynamo_attribute :user_name, :s
+  dynamo_attribute :lat, :n
+  dynamo_attribute :long, :n
 end
